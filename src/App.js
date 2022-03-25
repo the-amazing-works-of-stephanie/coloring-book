4ab2abfb060da79754209cec05d55bffaf01513d
Stephanie <tsuyunoinochi@gmail.com>
2022-03-25 19:26:14 -0400

    .

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, {useState} from "react";
 import './App.css';
-// import Navbar from './navbar';
 import { BrowserRouter as Router, Routes, Route}
     from 'react-router-dom';
 import Home from "./pages";
